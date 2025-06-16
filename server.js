d15f8e9a70ff61b50c9df50c1a83938758b0d1ab
Giovanni Santacroce <giovannisantacroce16@gmail.com>
2025-06-16 23:32:09 +0200

    Rename 'public' folder to 'docs' and update server.js accordingly

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the 'docs' directory
+app.use(express.static(path.join(__dirname, 'docs')));
 
 // Start the server
 app.listen(PORT, () => {
